e6dc18d2f1227c17f8ae2ceb2230c5fd435678da
Justin Love <git@JustinLove.name>
2015-09-12 06:04:47 -0500

    add icon to ffa players

--- ui/mods/chat_with_player/live_game_players.js
+++ ui/mods/chat_with_player/live_game_players.js
@@ -5,8 +5,13 @@
     })
   }
 
-  $('.me .div_player_name').replaceWith('<div data-bind="foreach: slots">'+
-    '<img src="coui://ui/main/shared/img/social/chat.png" data-bind="click: function(slot, e) {$root.chatWithPlayer(slot); e.stopPropagation()}"></img>'+
+  $('.me .div_player_name').replaceWith('<div data-bind="foreach: slots" class="chat_with_player_container">'+
+    '<img class="chat_with_player" src="coui://ui/main/shared/img/social/chat.png" data-bind="click: function(slot, e) {$root.chatWithPlayer(slot); e.stopPropagation()}"></img>'+
     '<div class="div_player_name" data-bind="text: $data, style: { color: $parent.disconnected ? \'DarkGray\' : \'White\', color: $parent.defeated ? \'Red\' + $parent.text - $parent.shadow : $parent.defeated ? \'0px 0px 10px #f00\' : \'\' }"></div>'+
+  '</div>')
+
+  $('.tbl_alliance_mode .div_player_name.truncate').replaceWith('<div data-bind="foreach: slots" class="chat_with_player_container">'+
+    '<img class="chat_with_player" src="coui://ui/main/shared/img/social/chat.png" data-bind="visible: !$parent.ai, click: function(slot, e) {$root.chatWithPlayer(slot); e.stopPropagation()}"></img>'+
+    '<div class="div_player_name truncate" data-bind="text: $data, css: { color_disconnected: $parent.disconnected, color_negative: $parent.defeated }">'+
   '</div>')
 })()
