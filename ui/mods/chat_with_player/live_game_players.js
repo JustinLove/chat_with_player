0a6ca11a0308ade1915a90eaf6c2d9b53ce17bf5
Justin Love <git@JustinLove.name>
2015-09-12 06:29:37 -0500

    spectator mode

--- ui/mods/chat_with_player/live_game_players.js
+++ ui/mods/chat_with_player/live_game_players.js
@@ -7,11 +7,21 @@
 
   $('.me .div_player_name').replaceWith('<div data-bind="foreach: slots" class="chat_with_player_container">'+
     '<img class="chat_with_player" src="coui://ui/main/shared/img/social/chat.png" data-bind="click: function(slot, e) {$root.chatWithPlayer(slot); e.stopPropagation()}"></img>'+
-    '<div class="div_player_name" data-bind="text: $data, style: { color: $parent.disconnected ? \'DarkGray\' : \'White\', color: $parent.defeated ? \'Red\' + $parent.text - $parent.shadow : $parent.defeated ? \'0px 0px 10px #f00\' : \'\' }"></div>'+
+    '<div class="div_player_name" data-bind="text: $data, style: { color: $parent.disconnected ? \'DarkGray\' : \'White\', color: $parent.defeated ? \'Red\' + $parent.text - $parent.shadow : $parent.defeated ? \'0px 0px 10px #f00\' : \'\' }"></div> '+
   '</div>')
 
-  $('.tbl_alliance_mode .div_player_name.truncate').replaceWith('<div data-bind="foreach: slots" class="chat_with_player_container">'+
+  $('.tbl_alliance_mode .div_player_name.truncate, .div_player_item.team .div_player_name.truncate').replaceWith('<div data-bind="foreach: slots" class="chat_with_player_container">'+
     '<img class="chat_with_player" src="coui://ui/main/shared/img/social/chat.png" data-bind="visible: !$parent.ai, click: function(slot, e) {$root.chatWithPlayer(slot); e.stopPropagation()}"></img>'+
-    '<div class="div_player_name truncate" data-bind="text: $data, css: { color_disconnected: $parent.disconnected, color_negative: $parent.defeated }">'+
+    '<div class="div_player_name truncate" data-bind="text: $data, css: { color_disconnected: $parent.disconnected, color_negative: $parent.defeated }"> '+
+  '</div>')
+
+  $('.div_spectator_player_info .color_negative').replaceWith('<div data-bind="foreach: slots" class="chat_with_player_container">'+
+    '<img class="chat_with_player" src="coui://ui/main/shared/img/social/chat.png" data-bind="visible: !$parent.ai, click: function(slot, e) {$root.chatWithPlayer(slot); e.stopPropagation()}"></img>'+
+    '<span data-bind="text: $data" class="color_negative"></span>'+
+  '</div>')
+
+  $('.div_spectator_player_info .color_normal').replaceWith('<div data-bind="foreach: slots" class="chat_with_player_container">'+
+    '<img class="chat_with_player" src="coui://ui/main/shared/img/social/chat.png" data-bind="visible: !$parent.ai, click: function(slot, e) {$root.chatWithPlayer(slot); e.stopPropagation()}"></img>'+
+    '<span data-bind="text: $data" class="color_normal"></span>'+
   '</div>')
 })()
