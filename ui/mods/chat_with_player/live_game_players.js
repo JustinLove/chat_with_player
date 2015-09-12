1adfb800202f3804e1669f4830bfd99de9f5fe60
Justin Love <git@JustinLove.name>
2015-09-12 05:25:22 -0500

    proof of concept chat icon

--- ui/mods/chat_with_player/live_game_players.js
+++ ui/mods/chat_with_player/live_game_players.js
@@ -1,2 +1,13 @@
 (function() {
+  model.chatWithPlayer = function(displayName) {
+    api.Panel.message('uberbar', 'sendChat', {
+      displayName: displayName,
+      message: '.',
+    })
+  }
+
+  $('.me .div_player_name').replaceWith('<div data-bind="foreach: slots">'+
+    '<img src="coui://ui/main/shared/img/social/chat.png" data-bind="click: function(slot, e) {$root.chatWithPlayer(slot); e.stopPropagation()}"></img>'+
+    '<div class="div_player_name" data-bind="text: $data, style: { color: $parent.disconnected ? \'DarkGray\' : \'White\', color: $parent.defeated ? \'Red\' + $parent.text - $parent.shadow : $parent.defeated ? \'0px 0px 10px #f00\' : \'\' }"></div>'+
+  '</div>')
 })()
